186812195655110704694fb0c7313a3c501aad68
Anton Kholomiov <anton.kholomiov@gmail.com>
2023-09-30 19:52:48 +0300

    Update http types structure

--- mig/src/Mig/Core.hs
+++ mig/src/Mig/Core.hs
@@ -10,5 +10,3 @@ import Mig.Core.Server as X
 import Mig.Core.Server.Class as X
 import Mig.Core.ServerFun as X
 import Mig.Core.Types as X
-import Mig.Core.Types.MediaType as X
-import Mig.Core.Types.Response as X
