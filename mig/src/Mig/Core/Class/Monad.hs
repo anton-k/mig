f365c391f4e7523284d586c2cca8d7f087883be6
Anton Kholomiov <a.kholomyev@tinkoff.ru>
2023-10-06 15:01:41 +0300

    Use common monad finder type-level function

---
+++ mig/src/Mig/Core/Class/Monad.hs
@@ -0,0 +1,15 @@
+-- | Type-level function to extract underlying server monad
+module Mig.Core.Class.Monad (
+  MonadOf,
+  Send (..),
+) where
+
+import Data.Kind
+import Mig.Core.Types.Http
+import Mig.Core.Types.Route
+
+type family MonadOf a :: (Type -> Type) where
+  MonadOf (Send method m a) = m
+  MonadOf (Request -> m (Maybe Response)) = m
+  MonadOf (f m) = m
+  MonadOf (a -> b) = MonadOf b
