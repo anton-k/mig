0fd193ff3529c9d0c49c7f744e35d85b8e2d1ef8
Anton Kholomiov <anton.kholomiov@gmail.com>
2023-11-04 11:08:33 +0300

    Fix formatting

--- mig/test/Test/Server.hs
+++ mig/test/Test/Server.hs
@@ -1,9 +1,9 @@
 module Test.Server (spec) where
 
+import Test.Hspec
+import Test.Server.Counter qualified as Counter
 import Test.Server.Hello qualified as Hello
 import Test.Server.RouteArgs qualified as RouteArgs
-import Test.Server.Counter qualified as Counter
-import Test.Hspec
 
 spec :: Spec
 spec = describe "server" $ do
